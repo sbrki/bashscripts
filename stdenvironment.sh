823e517cdc8894ffd11576bfcc3c4d12a000da6d
whiteShtef <stjepanbrkicc@gmail.com>
2016-06-20 19:05:12 +0200

    Add files via upload
    Standard script that downloads all the software that i need when I move to a new distribution.

---
+++ stdenvironment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+#programming stuff
+add-apt-repository ppa:webupd8team/sublime-text-3
+apt-get update
+apt-get install sublime-text-installer
+apt-get install python3 -y
+apt-get install pip3 -y
+pip3 install flask
+pip3 install peewee
+#other stuff
+apt-get install vlc
+apt-get install google-chrome-stable
